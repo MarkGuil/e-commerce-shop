792142be752930e65af8131e62f40111341536d9
MarkGuil <92792417+MarkGuil@users.noreply.github.com>
2023-05-04 16:04:30 +0800

    Update anishop.sql

--- Database/anishop.sql
+++ Database/anishop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2023 at 11:30 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: May 04, 2023 at 10:03 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cartID` int(100) NOT NULL,
+  `customerID` int(100) NOT NULL,
+  `productID` int(255) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `totalPrice` double NOT NULL,
+  `statID` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `category`
 --
 
@@ -31,7 +46,7 @@ CREATE TABLE `category` (
   `category_id` int(225) NOT NULL,
   `category_name` varchar(225) NOT NULL,
   `picture` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -45,10 +60,10 @@ CREATE TABLE `customer` (
   `username` varchar(50) NOT NULL,
   `phone` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(250) NOT NULL,
   `address` varchar(225) NOT NULL,
   `postalCode` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -64,7 +79,7 @@ CREATE TABLE `order_page` (
   `totalPrice` double NOT NULL,
   `statID` int(100) NOT NULL,
   `voucher_stat` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -75,7 +90,7 @@ CREATE TABLE `order_page` (
 CREATE TABLE `prodstatus` (
   `statID` int(100) NOT NULL,
   `statusName` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -91,9 +106,10 @@ CREATE TABLE `product` (
   `picture` varchar(225) NOT NULL,
   `status` varchar(50) NOT NULL,
   `price` double NOT NULL,
+  `availableQuantity` int(11) NOT NULL DEFAULT 0,
   `voucher` int(11) NOT NULL,
   `vouchervalue` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -106,7 +122,7 @@ CREATE TABLE `user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -119,11 +135,20 @@ CREATE TABLE `voucher` (
   `product_id` int(11) NOT NULL,
   `value` int(11) NOT NULL,
   `voucher_value` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cartID`),
+  ADD KEY `statID` (`statID`),
+  ADD KEY `customerID` (`customerID`),
+  ADD KEY `productID` (`productID`);
 
 --
 -- Indexes for table `category`
@@ -176,6 +201,12 @@ ALTER TABLE `voucher`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cartID` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `category`
